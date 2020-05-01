c306a170cecf802f2b27377f5d4d50b9076141fe
wanderling <poobeshgowtham@live.com>
2020-05-01 17:12:14 +0530

    notes

---
+++ __/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCgz4_-_3iLe6SsAcPfpLkBPYdb4w0M8OE",
+  "appId": "1:346428745929:web:ec65b7b4e48e0d548fc518",
+  "authDomain": "poobeshgowtham.firebaseapp.com",
+  "databaseURL": "https://poobeshgowtham.firebaseio.com",
+  "measurementId": "G-FXXGL38B21",
+  "messagingSenderId": "346428745929",
+  "projectId": "poobeshgowtham",
+  "storageBucket": "poobeshgowtham.appspot.com"
+});
